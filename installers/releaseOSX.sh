ce251c14df5d7778031a8b284962cf974503e0f2
Patrick Werner <boon.werner@gmail.com>
2019-03-29 17:36:27 +0100

    fixed osx installer

--- installers/releaseOSX.sh
+++ installers/releaseOSX.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 cd ../OpenRobertaUSB && mvn clean install && cd ../installers
+cd osx && ./appify start.sh OpenRobertaUSB
+mkdir -p package/ORUSB.pkg/Payload/Applications/OpenRobertaUSB.app/ 
+cp -a OpenRobertaUSB.app package/ORUSB.pkg/Payload/Applications/
+pkgutil --flatten-full package OpenRobertaUSBMacOSX.pkg
+mv OpenRobertaUSBMacOSX.pkg ..
+rm -rf OpenRobertaUSB.app
